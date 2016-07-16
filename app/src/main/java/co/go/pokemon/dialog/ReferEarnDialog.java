4a514e3361ba798a9d4359340c766efc6533c5bb
Kapil Kapri <kapilkapri@gofynd.com>
2016-07-16 11:15:32 +0530

    started working on dialog

---
+++ app/src/main/java/co/go/pokemon/dialog/ReferEarnDialog.java
@@ -0,0 +1,28 @@
+package co.go.pokemon.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.go.pokemon.R;
+
+
+public class ReferEarnDialog extends DialogFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.dialog_refer_earn, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
